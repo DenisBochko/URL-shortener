69a23bad2e4e2cf4e8b3e4ea2e524e5fad857585
reElcy <reElcy@yandex.ru>
2025-01-10 01:23:21 +0300

    Добавлен и настроен функционал логгера

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,21 +1,55 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"url-shortener/internal/config"
+
+	"log/slog"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
 )
 
 func main() {
 	// init config: cleanenv
 	cfg := config.MustLoad()
 
-	fmt.Print(cfg)
+	// init logger: slog (import "log/slog") - библиотека для работы с различными логгерами
+	log := setupLogger(cfg.Env)
 
-	// init logger: slog (import "log/slog")
+	// Старт приложения, также выводится какое окружение используется
+	log.Info("Starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// init storage: sqlite
 
 	// init router: chi (полностью совместим с net/http), render
 
 	// run server
+}
+
+// Функция конфигурации логгера, зависит от входного параметра env, локально мы хотим видеть текстовые логи, на сервере,
+// т.е. в окружении dev или prod, мы хотим видеть логи в формате json
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	// если local, то выводим текстовые логи, если dev, то выводим json логи
+	switch env {
+	case envLocal:
+		// создаём логгер, использую текстовые хендлер
+		// Level обозначает минимальный уровень логов, который мы выводим
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		// создаём логгер, использую json хендлер
+		// Level обозначает минимальный уровень логов, который мы выводим
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	}
+
+	return log
 }
