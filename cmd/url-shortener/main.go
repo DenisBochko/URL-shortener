92074d84304707778a6ef645666db000e80525fb
reElcy <reElcy@yandex.ru>
2025-01-10 00:18:41 +0300

    Первый коммит, добавлена структура проекта, config, парсер yaml файла с конфигом

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,14 @@
+package main
+
+
+func main() {
+	// init config: cleanenv
+
+	// init logger: slog (import "log/slog")
+
+	// init storage: sqlite
+
+	// init router: chi (полностью совместим с net/http), render 
+
+	// run server
+}
---
+++ internal/config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	// struct tag yaml определяет, какое имя у соответствующего параметра будет в yaml файле
+	// struct tag env определяет, какое имя будет у переменной окружения, если мы будем использовать её
+	// struct tag env-default определяет дефолтное значение
+	Env         string `yaml:"env" env:"ENV" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env:"storage_path" env-required:"true"`
+	// Встраиваем структуру HTTPServer в общий конфиг
+	HTTPServer `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Addres      string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+// функии с приставкой Must не возвращают ошибку, а паникуют, так делать можно в редких случаях, например, при запуску приложения
+func MustLoad() *Config {
+	// Получаем путь до файла конфигураций из переменной окружения
+	configPath := os.Getenv("CONFIG_PATH")
+	// Роняем приложение, если не получили путь к конфигу
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	// проверяем, существует ли файл по данному пути
+	if _, err := os.Stat(configPath); os.IsNotExist(err) { // IsNotExist возвращает true, если файл не существует
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	// Читаем конфиг-файл и заполняем нашу структуру
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("error reading config file: %s", err)
+	}
+
+	return &cfg
+}
